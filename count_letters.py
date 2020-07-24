cc679565c410b9dee46f23fe127431c1f55b2ec6
clara0 <clara.fang@gmail.com>
2020-07-24 15:37:03 -0400

    Initial commit

---
+++ count_letters.py
@@ -0,0 +1,21 @@
+# count letters in a file
+import collections
+
+
+def countLetters(file):
+    letters = dict()
+
+    for line in file:
+        line = line.lower()
+        for char in line:
+            if char.isalpha():
+                letters[char] = letters.get(char, 0) + 1
+
+    sortedDict = sorted(letters.items(), key=lambda x: x[1])
+    return sortedDict
+
+
+fileHandle = open('/tmp/wget.rb')
+x = countLetters(fileHandle)
+print(x)
+
