40cc42725ce44f24bc05b83ae87b44302b26d9eb
clara0 <clara.fang@gmail.com>
2020-07-26 13:56:27 -0400

    changed output to a tabular format

--- personal_info.py
+++ personal_info.py
@@ -33,5 +33,12 @@ def readInfo(file):
 fileHandle = open('/Users/clara/Downloads/dataJul-26-2020.csv')
 people = readInfo(fileHandle)
 
+print('Name\t\t\t\t\t\t\tEmail\t\t\t\t\t\t\t\tPhone\t\t\t\t\t\t\tZip Code')
 for person in people:
-    print(person)
+    for i in person:
+        if i == person[-1]:
+            print(i[:20], end='\n')
+        else:
+            print(i[:20], end='\t\t\t\t\t\t')
+
+
