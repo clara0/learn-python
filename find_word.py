7b66e92214bed1f1de8d0f5d95e07406e7db5267
clara0 <clara.fang@gmail.com>
2020-08-05 15:56:39 -0400

    Fixed a problem regarding the options

--- find_word.py
+++ find_word.py
@@ -3,17 +3,20 @@ import sys
 
 
 def findWord(word, file):
-    ignoreCase = False
     arguments = sys.argv
     lineList = []
     if len(arguments) < 3:
         raise Exception('usage: [word] [-i ignore case] [--ignore-case ignore case] [file]')
-    if '-i' or '--ignore-case' in arguments:
+    if arguments[1] == '-i' or '--ignore-case' and len(arguments) == 4:
         ignoreCase = True
+    else:
+        ignoreCase = False
     if ignoreCase:
         for line1 in file:
-            if (word.lower() or word.title() or word.upper()) in line1:
-                lineList.append(line1)
+            words = line1.split()
+            for w in words:
+                if w.lower() == word.lower():
+                    lineList.append(line1)
     else:
         for line1 in file:
             if word in line1:
@@ -27,6 +30,6 @@ try:
 except FileNotFoundError:
     raise Exception('usage: [word] [-i ignore case] [--ignore-case ignore case] [file]')
 
-x = findWord(sys.argv[1], fhand)
+x = findWord(sys.argv[(-1) - 1], fhand)
 for line in x:
     print(line)
