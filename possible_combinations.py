67692a5ac4d6460ef93431dcb28c26e0682b23f4
clara0 <clara.fang@gmail.com>
2020-08-10 14:35:21 -0400

    First commit for possible_combinations.py

---
+++ possible_combinations.py
@@ -0,0 +1,15 @@
+
+
+def findAll(tuple1, tuple2):
+    possibleCombos = []
+    for char1 in tuple1:
+        for char2 in tuple2:
+            possibleCombos.append(char1 + char2)
+            possibleCombos.append(char2 + char1)
+
+    return set(possibleCombos)
+
+
+if __name__ == "__main__":
+    combos = findAll(('1', '2', '3'), ('a', 'b', 'c'))
+    print(combos)
