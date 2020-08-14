80be7418d6dc049fa212053b69a412ee506a1969
clara0 <clara.fang@gmail.com>
2020-08-14 12:16:49 -0400

    Issue Add unit tests #4

---
+++ tests/test_countLetters.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import count_letters
+
+
+class TestCountLetters(unittest.TestCase):
+    def setUp(self):
+        fhand = open('/tmp/a.txt', 'w+')
+        fhand.write('aaabbc')
+        fhand.close()
+
+    def tearDown(self):
+        os.remove('/tmp/a.txt')
+
+    def test_countLetters(self):
+        fhand = open('/tmp/a.txt')
+        dictionary = count_letters.countLetters(fhand)
+        print(dictionary)
+        self.assertEqual(dictionary[0], ('c', 1))
+        self.assertEqual(dictionary[1], ('b', 2))
+        self.assertEqual(dictionary[2], ('a', 3))
+
---
+++ tests/test_listFiles.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+import shutil
+import list_files
+
+
+class TestListFiles(unittest.TestCase):
+    def setUp(self):
+        os.makedirs('/tmp/a/b/c')
+        os.mkdir('/tmp/a/aa')
+        fhand1 = open('/tmp/a/under_a.txt', 'w+')
+        fhand2 = open('/tmp/a/b/under_b.txt', 'w+')
+        fhand1.close()
+        fhand2.close()
+
+    def tearDown(self):
+        shutil.rmtree('/tmp/a')
+
+    def test_listFiles(self):
+        listedContents = list_files.listFiles('/tmp/a', [], recursive=True)
+        self.assertEqual(listedContents, [['aa'], 'under_a.txt', ['b', 'under_b.txt', 'c']])
+
+        listedContents = list_files.listFiles('/tmp/a', [])
+        self.assertEqual(listedContents, ['aa', 'under_a.txt', 'b'])
+
