c38da19afa0c399a6cadeb4bbd141c6fb5adab18
clara0 <clara.fang@gmail.com>
2020-09-10 13:29:28 -0400

    Stripped word variable and returned word inside loop

--- triple_double_letters.py
+++ triple_double_letters.py
@@ -14,17 +14,18 @@ def findTripleDoubles(word):
                 inbetween = 0
         elif 0 < pairs < 3:
             inbetween += 1
+        if pairs == 3:
+            return True
         prevLetter = letter
-    if pairs == 3:
-        return word
+    return False
 
 
 if __name__ == '__main__':
     file = urlopen('http://www.gutenberg.org/files/3201/files/CROSSWD.TXT')
     resultList = []
     for line in file:
-        word = line.decode('utf-8')
+        word = (line.decode('utf-8')).strip()
         result = findTripleDoubles(word)
-        if result is not None:
+        if result is not False:
             resultList.append(word)
     print(resultList)
