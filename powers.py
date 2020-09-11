ae9c0ee00b17aae672cfd96fad35c04ecccbaf61
clara0 <clara.fang@gmail.com>
2020-09-11 12:00:49 -0400

    Inline variables with only one occurrence

--- powers.py
+++ powers.py
@@ -7,8 +7,7 @@ def isPower(baseNum, powerNum):
             if powerNum == baseNum:
                 return True
             else:
-                result = isPower(baseNum, powerNum / baseNum)
-                return result
+                return isPower(baseNum, powerNum / baseNum)
         else:
             return False
 
@@ -20,5 +19,4 @@ if __name__ == '__main__':
     while True:
         firstNum = input('Enter a base number: ').strip()
         secondNum = input('Enter another number: ').strip()
-        result = isPower(float(firstNum), float(secondNum))
-        print(f'{secondNum} is a power of {firstNum}? {result}')
+        print(f'{secondNum} is a power of {firstNum}? {isPower(float(firstNum), float(secondNum))}')
--- tests/test_powers.py
+++ tests/test_powers.py
@@ -4,20 +4,9 @@ import powers
 
 class TestPowers(unittest.TestCase):
     def test_powers(self):
-        result1 = powers.isPower(10, 100)
-        self.assertEqual(result1, True)
-
-        result2 = powers.isPower(11, 1331)
-        self.assertEqual(result2, True)
-
-        result3 = powers.isPower(2, 8)
-        self.assertEqual(result3, True)
-
-        result4 = powers.isPower(0, 100)
-        self.assertEqual(result4, False)
-
-        result5 = powers.isPower(1, 0)
-        self.assertEqual(result5, False)
-
-        result6 = powers.isPower('blah', 1)
-        self.assertEqual(result6, False)
+        self.assertEqual(powers.isPower(10, 100), True)
+        self.assertEqual(powers.isPower(11, 1331), True)
+        self.assertEqual(powers.isPower(2, 8), True)
+        self.assertEqual(powers.isPower(0, 100), False)
+        self.assertEqual(powers.isPower(1, 0), False)
+        self.assertEqual(powers.isPower('blah', 1), False)
