b3bffdeb4650accfd643a43ee927bb7815f3e031
clara0 <clara.fang@gmail.com>
2020-09-12 17:14:15 -0400

    initial impl for rotate_word.py

---
+++ rotate_word.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import string
+
+alphabet = string.ascii_lowercase
+
+
+def rotateLetter(letter, rotateNum):
+    if letter in alphabet:
+        newLetterIndex = alphabet.find(letter) + rotateNum
+        finalNewLetterIndex = newLetterIndex - (newLetterIndex // len(alphabet)) * len(alphabet)
+        return alphabet[finalNewLetterIndex]
+
+
+def rotate(word, rotateNum):
+    newWord = ''
+
+    for letter in word.lower():
+        newWord = newWord + (rotateLetter(letter, rotateNum))
+
+    return newWord
+
+
+if __name__ == '__main__':
+    while True:
+        string = input('Enter a word: ').strip()
+        if string.isalpha():
+            num = input('Enter a number: ').strip()
+            if num.isnumeric():
+                result = rotate(string, int(num))
+                print(result)
+
+
+
