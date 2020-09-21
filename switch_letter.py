9a3b9faa872e07746e1b93618afba06e04b0f539
clara0 <clara.fang@gmail.com>
2020-09-21 19:51:50 -0400

    Made program more efficient

--- switch_letter.py
+++ switch_letter.py
@@ -4,16 +4,14 @@ from urllib.request import urlopen
 
 
 def isValidWordPair(word1, word2):
-    if sorted(word1) == sorted(word2):
-        differentLetters = 0
-        for letterIndex, letter1 in enumerate(word1):
-            if letter1 != word2[letterIndex]:
-                differentLetters += 1
-            if differentLetters >= 3:
-                return False
+    differentLetters = 0
+    for letterIndex, letter1 in enumerate(word1):
+        if letter1 != word2[letterIndex]:
+            differentLetters += 1
+        if differentLetters >= 3:
+            return False
 
         if differentLetters == 2:
-            print('match!', word1, word2)
             return True
     return False
 
@@ -23,12 +21,16 @@ if __name__ == '__main__':
     file = urlopen('http://www.gutenberg.org/files/3201/files/CROSSWD.TXT')
     wordList = sorted([line.decode('utf-8').strip() for line in file], key=len)
 
-    for index, word1 in enumerate(wordList):
+    for index, word1 in enumerate(wordList[:3000]):
         len1 = len(word1)
-        for word2 in wordList[index + 1:]:
+        sorted1 = sorted(word1)
+        for word2 in wordList[index + 1: 3000]:
             if len(word2) > len1:
                 break
-            if isValidWordPair(word1, word2):
-                wordPairList.append((word1, word2))
+            sorted2 = sorted(word2)
+            if sorted1 == sorted2:
+                if isValidWordPair(word1, word2):
+                    wordPairList.append((word1, word2))
 
-    print(wordPairList)
+    for a, b in wordPairList:
+        print(a, b)
