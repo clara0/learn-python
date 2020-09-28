fa21ce2e01f9837abb341038dd88971b5b8f58b5
clara0 <clara.fang@gmail.com>
2020-09-28 19:39:21 -0400

    initial impl for dna.py

---
+++ dna.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def matchLetters(letters):
+    pairList = []
+    for letter in letters:
+        if letter == 'A':
+            pairList.append((letter, 'T'))
+        elif letter == 'T':
+            pairList.append((letter, 'A'))
+        elif letter == 'G':
+            pairList.append((letter, 'C'))
+        elif letter == 'C':
+            pairList.append((letter, 'G'))
+        else:
+            raise Exception('Enter a valid set of numbers')
+
+    return pairList
+
+
+if __name__ == '__main__':
+    while True:
+        userInput = input('Enter some DNA letters: ').strip().upper()
+        try:
+            if userInput != '':
+                print(matchLetters(userInput))
+        except:
+            continue
