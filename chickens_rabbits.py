3e897dc19ce187f5257fbb4f586f27e68e16e892
clara0 <clara.fang@gmail.com>
2020-10-02 20:48:27 -0400

    initial impl for chickens_rabbits.py

---
+++ chickens_rabbits.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# 35 total heads
+# 94 feet
+# how many rabbits and chickens
+
+rabbits = (94 - (35 * 2)) / 2
+chickens = 35 - rabbits
+
+print(f'There are {rabbits} rabbits and {chickens} chickens.')
