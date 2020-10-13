f11ae5589bbb8439512823b46618a6ee6bcb0d03
clara0 <clara.fang@gmail.com>
2020-10-12 21:09:23 -0400

    initial impl for check_phone_number.py and test_checkPhoneNumber.py

---
+++ check_phone_number.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+def checkPhoneNum(phoneNum):
+    notValid = '~`!@#$%^&*_+={}[]|\\:;\'"?/>.<,qwertyuiopasdfghjklzxcvbnm'
+    numbers = 0
+    for i in phoneNum:
+        if i.isnumeric():
+            numbers += 1
+    if 9 < numbers < 12:
+        tripleNumCount = 0
+        for count, i in enumerate(phoneNum):
+            if i in notValid:
+                return False
+
+            if tripleNumCount == 2 and phoneNum[-4:].isnumeric():
+                if numbers == 11:
+                    if phoneNum.startswith('1'):
+                        return True
+                    else:
+                        return False
+                return True
+
+            elif tripleNumCount < 2 and phoneNum[count:count + 3].isnumeric():
+                tripleNumCount += 1
+    return False
+
+
+if __name__ == '__main__':
+    while True:
+        userInput = input('Enter a U.S. phone number: ').strip()
+        result = checkPhoneNum(userInput)
+        print(result)
---
+++ tests/test_checkPhoneNumber.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+import check_phone_number
+
+
+class TestCheckPhoneNumber(TestCase):
+    def test_checkPhoneNum(self):
+        self.assertTrue(check_phone_number.checkPhoneNum('555-555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('(555)555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('(555) 555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('555 555 5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('5555555555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('1 555-555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('1(555) 555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('1(555)555-5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('1(555) 555 5555'))
+        self.assertTrue(check_phone_number.checkPhoneNum('1 552 235 5490'))
+        self.assertFalse(check_phone_number.checkPhoneNum('2(555) 555 5555'))
+        self.assertFalse(check_phone_number.checkPhoneNum('1jjj 555 5555'))
+        self.assertFalse(check_phone_number.checkPhoneNum('1 234234 555 5555'))
+        self.assertFalse(check_phone_number.checkPhoneNum('2( 555 5555'))
+        self.assertFalse(check_phone_number.checkPhoneNum('2555) 555 5555'))
+        self.assertFalse(check_phone_number.checkPhoneNum('(1(555) 555 5555)'))
