6ba964c1d470e67dd3092130b4410baee3544d61
clara0 <clara.fang@gmail.com>
2020-10-14 21:00:03 -0400

    initial impl for abs_value.py

---
+++ abs_value.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import random
+
+    while True:
+        numRange = range(-100, 101)
+        randomNum = random.choice(numRange)
+        answer = str(abs(randomNum) * 2)
+        while True:
+            userInput = input(f'Enter double the amount of the absolute value of {randomNum}: ').strip()
+            if userInput == answer:
+                print('Correct!')
+                break
+            else:
+                print('Try again!')
