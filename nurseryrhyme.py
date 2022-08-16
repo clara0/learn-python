4317970a987dce4159837b3469015329f1185713
Clara Fang <clara.fang@gmail.com>
2022-08-16 14:11:11 -0400

    initial impl for nurseryrhyme.py

---
+++ nurseryrhyme.py
@@ -0,0 +1,8 @@
+
+nums = ['ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two']
+
+for i in nums:
+    print('There were ' + str(i) + ' in a bed and the little one said "Roll over! Roll over!" So they all rolled over and one fell out.')
+
+print('There was one in the bed and the little one said, "Alone at last! Good night!"')
+
